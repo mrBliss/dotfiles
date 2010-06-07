1acc4346d36634525fb255c7d5ec3b2a26fd53f2
mrBliss <dewinant@gmail.com>
2010-06-07 20:09:04 +0200

    Removed some unnecessary colors stuff appearance.zsh

--- .oh-my-zsh/lib/appearance.zsh
+++ .oh-my-zsh/lib/appearance.zsh
@@ -36,23 +36,3 @@ setopt prompt_subst
 
 # Load the theme
 source "$ZSH/themes/$ZSH_THEME.zsh-theme"
-
-# A script to make using 256 colors in zsh less painful.
-# P.C. Shyamshankar <sykora@lucentbeing.com>
-# Copied from http://github.com/sykora/etc/blob/master/zsh/functions/spectrum/
-
-typeset -Ag FX FG BG
-
-FX=(
-    reset     "%{[00m%}"
-    bold      "%{[01m%}" no-bold      "%{[22m%}"
-    italic    "%{[03m%}" no-italic    "%{[23m%}"
-    underline "%{[04m%}" no-underline "%{[24m%}"
-    blink     "%{[05m%}" no-blink     "%{[25m%}"
-    reverse   "%{[07m%}" no-reverse   "%{[27m%}"
-)
-
-for color in {000..255}; do
-    FG[$color]="%{[38;5;${color}m%}"
-    BG[$color]="%{[48;5;${color}m%}"
-done
