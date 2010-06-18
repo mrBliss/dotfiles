e790cc9d7376e796c86959dc4f98fc9e951a04b3
mrBliss <dewinant@gmail.com>
2010-06-18 14:48:32 +0200

    Added remove-M, a function that removes the ^M and the end of each line

--- .emacs.d/functions.el
+++ .emacs.d/functions.el
@@ -172,4 +172,18 @@ from http://amitp.blogspot.com/2007/03/emacs-dont-kill-unsaved-buffers.html."
   (interactive)
   (diff-buffer-with-file (current-buffer)))
 
+(defun remove-M ()
+  "Remove ^M at end of line in the whole buffer."
+  (interactive)
+  (save-match-data
+    (save-excursion
+      (let ((remove-count 0))
+        (goto-char (point-min))
+        (while (re-search-forward "
+" (point-max) t)
+          (setq remove-count (+ remove-count 1))
+          (replace-match "
+" nil nil))
+        (message (format "%d ^M removed from buffer." remove-count))))))
+
 (provide 'functions)
