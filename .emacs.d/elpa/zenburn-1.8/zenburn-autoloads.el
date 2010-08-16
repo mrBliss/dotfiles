e5750bcdd14cc02ca4b7091c4cb5a94051c92145
mrBliss <dewinant@gmail.com>
2010-08-16 12:13:45 +0200

    Emacs: Updated lua-mode from ELPA Installed zenburn theme from ELPA

---
+++ .emacs.d/elpa/zenburn-1.8/zenburn-autoloads.el
@@ -0,0 +1,28 @@
+;;; zenburn-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (color-theme-zenburn) "zenburn" "zenburn.el" (19560
+;;;;;;  30373))
+;;; Generated autoloads from zenburn.el
+
+(autoload 'color-theme-zenburn "zenburn" "\
+Just some alien fruit salad to keep you in the zone.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("zenburn-pkg.el") (19560 30373 222000))
+
+;;;***
+
+(provide 'zenburn-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zenburn-autoloads.el ends here
---
+++ .emacs.d/elpa/zenburn-1.8/zenburn-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zenburn" "1.8" "just some alien fruit salad to keep you in the zone" (quote nil))
