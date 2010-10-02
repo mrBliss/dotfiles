fd1ce5e48a11b6ad176b67ea040d0e5233ec6483
Thomas Winant <dewinant@gmail.com>
2010-10-02 16:12:36 +0200

    Found a better way ssh/scp to KULeuven

--- .oh-my-zsh/lib/functions.zsh
+++ .oh-my-zsh/lib/functions.zsh
@@ -11,7 +11,7 @@ mkcd () {
 
 # Connect to KULeuven
 kulssh () {
-    ssh -t s0202013@st.cs.kuleuven.be ssh s0202013@$1.cs.kotnet.kuleuven.be
+    ssh -A -t s0202013@st.cs.kuleuven.be ssh -A  s0202013@$1.cs.kotnet.kuleuven.be
 }
 
 # Extract files
