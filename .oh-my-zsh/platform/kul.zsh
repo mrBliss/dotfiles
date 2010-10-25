23fe1c0a0cd60a47cc7f0b1e7ccccf6cc9fe240e
mrBliss <dewinant@gmail.com>
2010-10-25 18:13:27 +0200

    Added some handy aliases to kul.zsh

--- .oh-my-zsh/platform/kul.zsh
+++ .oh-my-zsh/platform/kul.zsh
@@ -13,8 +13,16 @@ alias sicstus="/cw/prolog/sicstus/sicstus-yes.linux/bin/sicstus-3.12.2 --goal ma
 # No root access so put $HOME/bin on the path
 export PATH=$HOME/bin/bin.linux/bin:$PATH
 
-# Run zsh
-alias zsh=$HOME/bin/bin.linux/bin/zsh
-
 # Configure lejos
 source /localhost/packages/lejos/bash_lejos
+export NXJ_HOME=/localhost/packages/lejos/lejos_nxj
+export JAVA_HOME=/usr/lib/jvm/java-6-sun
+
+# Compile and upload a NXJ Class
+nx () {
+    nxjc "$1.java"
+    nxj "$1"
+}
+
+# Run Visual Paradigm
+alias vispara=/localhost/packages/visual_paradigm/VP_Suite5.0/bin/Visual_Paradigm_for_UML_8.0
