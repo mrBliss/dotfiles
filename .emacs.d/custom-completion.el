9dd245ac1b5c267aba909f76a409faf91d139204
mrBliss <dewinant@gmail.com>
2011-06-09 20:53:01 +0200

    Show the buffer sizes in Ibuffer in a human-readable format.

--- .emacs.d/custom-completion.el
+++ .emacs.d/custom-completion.el
@@ -63,6 +63,28 @@ imenu index, then jumps to that symbol's location."
 ;; Always rescan the file before displaying imenu
 (setq imenu-auto-rescan t)
 
+;; Use human readable Size column instead of original one
+(define-ibuffer-column size-h
+  (:name "Size" :inline t)
+  (cond
+   ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+   (t (format "%8d" (buffer-size)))))
+
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)
+        (mark " "
+              (name 16 -1)
+              " "
+              filename)))
+
 ;;##############################################################################
 ;; auto-complete
 
