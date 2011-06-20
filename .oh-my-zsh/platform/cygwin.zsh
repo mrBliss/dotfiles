2dfd8479c7c1601b88e9eff83733246b2cc23987
mrBliss <dewinant@gmail.com>
2011-06-20 18:01:50 +0200

    Don't print escape codes for the color scheme of mintty in Emacs' ansi-term on Windows.

--- .oh-my-zsh/platform/cygwin.zsh
+++ .oh-my-zsh/platform/cygwin.zsh
@@ -7,24 +7,28 @@ export ALTERNATE_EDITOR='emacs --daemon; emacsclient -t'
 # Needed for ssh (ProxyCommand)
 export SHELL=/usr/bin/zsh
 
-# Change colours
-echo -e "\e]P0000000"
-echo -e "\e]P1FF0000"
-echo -e "\e]P2A6E22E"
-echo -e "\e]P3FD971F"
-echo -e "\e]P40074E8"
-echo -e "\e]P5F92672"
-echo -e "\e]P666D9EF"
-echo -e "\e]P7F8F8F0"
-echo -e "\e]P8444444"
-echo -e "\e]P9960050"
-echo -e "\e]PA61CE3C"
-echo -e "\e]PBFBDE2D"
-echo -e "\e]PC6D9CBE"
-echo -e "\e]PDAE59AB"
-echo -e "\e]PE89BDFF"
-echo -e "\e]PFCCCDDD"
-
+if [ "$TERM" != "eterm-color" ]
+then
+    # Change colours
+    echo -e "\e]P0000000"
+    echo -e "\e]P1FF0000"
+    echo -e "\e]P2A6E22E"
+    echo -e "\e]P3FD971F"
+    echo -e "\e]P40074E8"
+    echo -e "\e]P5F92672"
+    echo -e "\e]P666D9EF"
+    echo -e "\e]P7F8F8F0"
+    echo -e "\e]P8444444"
+    echo -e "\e]P9960050"
+    echo -e "\e]PA61CE3C"
+    echo -e "\e]PBFBDE2D"
+    echo -e "\e]PC6D9CBE"
+    echo -e "\e]PDAE59AB"
+    echo -e "\e]PE89BDFF"
+    echo -e "\e]PFCCCDDD"
+else
+    export TERM=xterm-256color
+fi
 # Keychain
 eval `keychain --eval --nogui -Q -q id_rsa`
 
