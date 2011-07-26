a1243c9993e7efd5ced6c582fcf9fd6bef5b826b
mrBliss <dewinant@gmail.com>
2011-07-26 18:12:22 +0200

    Use hippie-expand instead of dabbrev-expand.

--- .emacs.d/custom-completion.el
+++ .emacs.d/custom-completion.el
@@ -12,6 +12,9 @@
 (setq icomplete-prospects-height 1
       icomplete-compute-delay 0)
 
+;; Use hippie-expand instead of dabbrev-expand
+(global-set-key (kbd "M-/") 'hippie-expand)
+
 ;;##############################################################################
 ;; Abbrev
 
@@ -63,27 +66,6 @@ imenu index, then jumps to that symbol's location."
 ;; Always rescan the file before displaying imenu
 (setq imenu-auto-rescan t)
 
-;; Use human readable Size column instead of original one
-(define-ibuffer-column size-h
-  (:name "Size" :inline t)
-  (cond
-   ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
-   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
-   (t (format "%8d" (buffer-size)))))
-
-(setq ibuffer-formats
-      '((mark modified read-only " "
-              (name 18 18 :left :elide)
-              " "
-              (size-h 9 -1 :right)
-              " "
-              (mode 16 16 :left :elide)
-              " "
-              filename-and-process)
-        (mark " "
-              (name 16 -1)
-              " "
-              filename)))
 
 ;;##############################################################################
 ;; auto-complete
@@ -173,6 +155,28 @@ imenu index, then jumps to that symbol's location."
 
 ;; Don't show empty groups
 (setq ibuffer-show-empty-filter-groups nil)
+
+;; Use human readable Size column instead of original one
+(define-ibuffer-column size-h
+  (:name "Size" :inline t)
+  (cond
+   ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+   (t (format "%8d" (buffer-size)))))
+
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)
+        (mark " "
+              (name 16 -1)
+              " "
+              filename)))
 
 (defun recentf-ido-find-file ()
   "Find a recent file using Ido."
