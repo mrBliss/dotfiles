8520fa107cfd93ef1278058747c53cea90725524
mrBliss <dewinant@gmail.com>
2011-10-22 15:18:46 +0200

    Update ac-slime to latest version.

--- .emacs.d/vendor/ac-slime.el
+++ .emacs.d/vendor/ac-slime.el
@@ -1,13 +1,17 @@
-;;----------------------------------------------------------------------------
-;; An auto-complete source using slime completions
+;;; ac-slime.el --- An auto-complete source using slime completions
 ;;
-;; Author: Steve Purcell <steve at sanityinc dot com>
+;;; Author: Steve Purcell <steve@sanityinc.com>
+;;; URL: https://github.com/purcell/ac-slime
+;;; Version: 0.1
 ;;
+;;; Commentary:
 ;; Usage:
-;;   (require 'ac-slime)
-;;   (add-hook 'slime-mode-hook 'set-up-slime-ac)
-;;   (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-;;----------------------------------------------------------------------------
+;;     (require 'ac-slime)
+;;     (add-hook 'slime-mode-hook 'set-up-slime-ac)
+;;     (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+;;     (eval-after-load "auto-complete"
+;;       '(add-to-list 'ac-modes 'slime-repl-mode))
+;;
 
 (eval-when-compile (require 'cl))
 
@@ -15,8 +19,7 @@
   "Return a possibly-empty list of fuzzy completions for the symbol at point."
   (when (slime-connected-p)
     (let ((slime-fuzzy-completion-limit 50))
-      (mapcar 'car (car (slime-fuzzy-completions
-                         (substring-no-properties ac-prefix)))))))
+      (mapcar 'car (car (slime-fuzzy-completions (substring-no-properties ac-prefix)))))))
 
 (defun ac-source-slime-simple-candidates ()
   "Return a possibly-empty list of completions for the symbol at point."
@@ -31,16 +34,19 @@
 (defun ac-slime-init ()
   (setq ac-slime-current-doc nil))
 
+;;;###autoload
 (defface ac-slime-menu-face
   '((t (:inherit 'ac-candidate-face)))
   "Face for slime candidate menu."
   :group 'auto-complete)
 
+;;;###autoload
 (defface ac-slime-selection-face
   '((t (:inherit 'ac-selection-face)))
   "Face for the slime selected candidate."
   :group 'auto-complete)
 
+;;;###autoload
 (defvar ac-source-slime-fuzzy
   '((init . ac-slime-init)
     (candidates . ac-source-slime-fuzzy-candidates)
@@ -52,6 +58,7 @@
     (document . ac-slime-documentation))
   "Source for fuzzy slime completion")
 
+;;;###autoload
 (defvar ac-source-slime-simple
   '((init . ac-slime-init)
     (candidates . ac-source-slime-simple-candidates)
@@ -63,6 +70,7 @@
   "Source for slime completion")
 
 
+;;;###autoload
 (defun set-up-slime-ac (&optional fuzzy)
   "Add an optionally-fuzzy slime completion source to the
 front of `ac-sources' for the current buffer."
@@ -74,3 +82,4 @@ front of `ac-sources' for the current buffer."
 
 
 (provide 'ac-slime)
+;;; ac-slime.el ends here
