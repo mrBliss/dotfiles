f2dc81c103080c5c67ce9f7127b20d3ccc69c529
mrBliss <dewinant@gmail.com>
2011-11-01 16:18:14 +0100

    Put latex customisations inside a hook.

--- .emacs.d/custom-latex.el
+++ .emacs.d/custom-latex.el
@@ -4,6 +4,8 @@
 ;; Created: Sat Dec 11 2010
 ;; Keywords: latex, auctex, settings
 
+
+
 ;; Use flymake with chktex
 (defun flymake-get-tex-args (file-name)
   (list "chktex" (list "-q" "-v0" file-name)))
@@ -29,40 +31,20 @@
   (ispell-change-dictionary "dutch")
   (eproject-mode -1)
   (flyspell-mode 1)
-  (define-key LaTeX-mode-map (kbd "M-d") 'TeX-font)
-  (define-key LaTeX-mode-map (kbd "C-c C-p") 'flymake-goto-prev-error)
-  (define-key LaTeX-mode-map (kbd "C-c C-n") 'flymake-goto-next-error)
-  (define-key LaTeX-mode-map (kbd "C-c C-l") 'TeX-next-error))
-(add-hook 'TeX-mode-hook 'latex-hook)
+  (define-key TeX-mode-map (kbd "M-d") 'TeX-font)
+  (define-key TeX-mode-map (kbd "C-c C-p") 'flymake-goto-prev-error)
+  (define-key TeX-mode-map (kbd "C-c C-n") 'flymake-goto-next-error)
+  (define-key TeX-mode-map (kbd "C-c C-l") 'TeX-next-error)
 
-;; Use Zathura as default PDF viewer on GNU/Linux, Skim on OS X.
-(setq TeX-view-program-list
-      (list (if (eq system-type 'darwin)
-                '("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %q")
-              '("Zathura" "zathura %o"))))
+  ;; Use Zathura as default PDF viewer on GNU/Linux, Skim on OS X.
+  (setq TeX-view-program-list
+        (list (if (eq system-type 'darwin)
+                  '("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b %n %o %b")
+                '("Zathura" "zathura %o"))))
+  (push (list 'output-pdf (caar TeX-view-program-list)) TeX-view-program-selection))
 
-(unless (boundp 'TeX-view-program-selection)
-  (setq TeX-view-program-selection))
-(push (list 'output-pdf (caar TeX-view-program-list))
-      TeX-view-program-selection)
 
-;; Synctex for Skim
-(when (eq system-type 'darwin)
-
-  (defun skim-make-url ()
-    (concat
-     (TeX-current-line)
-     " "
-     (expand-file-name (funcall file (TeX-output-extension) t)
-                       (file-name-directory (TeX-master-file)))
-     " "
-     (buffer-file-name)))
-
-  (unless (fboundp 'TeX-expand-list)
-    (setq TeX-expand-list nil))
-  (add-to-list 'TeX-expand-list '("%q" skim-make-url))
-
-  (setq LaTeX-command "latex -synctex=1"))
+(add-hook 'LaTeX-mode-hook 'latex-hook)
 
 
 (provide 'custom-latex)
