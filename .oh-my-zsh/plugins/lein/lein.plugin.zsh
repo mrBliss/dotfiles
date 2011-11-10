325784b227fd7ff36da964da104194e0d458c51f
mrBliss <dewinant@gmail.com>
2011-11-10 14:59:30 +0100

    Updated zsh autocompletion for leiningen.

--- .oh-my-zsh/plugins/lein/lein.plugin.zsh
+++ .oh-my-zsh/plugins/lein/lein.plugin.zsh
@@ -1,28 +1,71 @@
+#compdef lein
 
-function _lein_commands() {
-    local ret=1 state
-    _arguments ':subcommand:->subcommand' && ret=0
+# Lein ZSH completion function
+# Drop this somewhere in your $fpath (like /usr/share/zsh/site-functions)
+# and rename it _lein
 
-    case $state in
-      subcommand)
-        subcommands=(
-          "clean:remove compiled files and dependencies from project"
-          "compile:ahead-of-time compile the project"
-          "deps:download and install all dependencies"
-          "help:display a list of tasks or help for a given task"
-          "install:install the project and its dependencies in your local repository"
-          "jar:create a jar file containing the compiled .class files"
-          "new:create a new project skeleton"
-          "pom:write a pom.xml file to disk for maven interop"
-          "test:run the project's tests"
-          "uberjar:Create a jar including the contents of each of deps"
-          "upgrade:upgrade leiningen to the latest stable release"
-          "version:print leiningen's version"
-        )
-        _describe -t subcommands 'leiningen subcommands' subcommands && ret=0
-    esac
-
-    return ret
+_lein() {
+  if (( CURRENT > 2 )); then
+    # shift words so _arguments doesn't have to be concerned with second command
+    (( CURRENT-- ))
+    shift words
+    # use _call_function here in case it doesn't exist
+    _call_function 1 _lein_${words[1]}
+  else
+    _values "lein command" \
+     "classpath[Print the classpath of the current project.]" \
+     "clean[Remove compiled class files and jars from project.]" \
+     "compile[Compile Clojure source into \.class files.]" \
+     "deploy[Build jar and deploy to remote repository.]" \
+     "deps[Download :dependencies and put them in :library-path.]" \
+     "help[Display a list of tasks or help for a given task.]" \
+     "install[Install current project or download specified project.]" \
+     "interactive[Enter an interactive task shell.]" \
+     "jack-in[Jack in to a Clojure SLIME session from Emacs.]" \
+     "jar[Package up all the project's files into a jar file.]" \
+     "javac[Compile Java source files.]" \
+     "new[Create a new project skeleton.]" \
+     "plugin[Manage user-level plugins.]" \
+     "pom[Write a pom.xml file to disk for Maven interop.]" \
+     "repl[Start a repl session either with the current project or standalone.]" \
+     "retest[Run only the test namespaces which failed last time around.]" \
+     "run[Run the project's -main function.]" \
+     "search[Search remote repositories.]" \
+     "swank[Launch swank server for Emacs to connect.]" \
+     "test[Run the project's tests.]" \
+     "test![Run a project's tests after cleaning and fetching dependencies.]" \
+     "trampoline[Run a task without nesting the project's JVM inside Leiningen's.]" \
+     "uberjar[Package up the project files and all dependencies into a jar file.]" \
+     "upgrade[Upgrade Leiningen to the latest stable release.]" \
+     "version[Print version for Leiningen and the current JVM.]"
+  fi
 }
 
-compdef _lein_commands lein
+_lein_plugin() {
+  _values "lein plugin commands" \
+    "install[Download, package, and install plugin jarfile into ~/.lein/plugins]" \
+    "uninstall[Delete the plugin jarfile: \[GROUP/\]ARTIFACT-ID VERSION]"
+}
+
+
+_lein_namespaces() {
+  if [ -d test ]; then
+    _values "lein valid namespaces" $(find $1 -type f -name "*.clj" -exec grep -E \
+                 '^\(ns[[:space:]]+\w+' '{}' ';' | awk '/\(ns[ ]*([A-Za-z\.]+)/ {print $2}')
+  fi
+}
+
+_lein_run() {
+  _lein_namespaces "src/"
+}
+
+_lein_test() {
+  _lein_namespaces "test/"
+}
+
+_lein_test!() {
+  _lein_namespaces "test/"
+}
+
+
+compdef _lein lein
