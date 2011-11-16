44b181cae18757079aa44120619feed980601010
mrBliss <dewinant@gmail.com>
2011-11-17 00:39:06 +0100

    In order to use ansi-color-for-comint-mode-on, ansi-color should be loaded.

--- .emacs.d/custom-shell.el
+++ .emacs.d/custom-shell.el
@@ -6,6 +6,7 @@
 
 
 ;; Allow shell colors
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
 ;; Add custom colors to (ansi-)term
