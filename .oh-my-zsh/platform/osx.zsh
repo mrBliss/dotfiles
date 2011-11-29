73df538ab7b56923c67dce550d5737052154ad39
mrBliss <dewinant@gmail.com>
2011-11-29 23:31:08 +0100

    Clean up Mac OS X specific Zsh settings.

--- .oh-my-zsh/platform/osx.zsh
+++ .oh-my-zsh/platform/osx.zsh
@@ -7,6 +7,7 @@ export SHELL=/bin/zsh
 alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
 alias emacsclient='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
 alias emd='/Applications/Emacs.app/Contents/MacOS/Emacs --daemon'
+alias em='emacsclient -t -a "emd"'
 
 # Use emacsclient in a terminal window or nano
 export EDITOR='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -t'
@@ -14,12 +15,8 @@ export ALTERNATE_EDITOR='/Applications/Emacs.app/Contents/MacOS/Emacs --daemon; 
 
 # Environment variables
 export JAVA_HOME=/Library/Java/Home
-export PATH=/usr/local/bin:/usr/local/sbin:~/.cljr/bin:$PATH
+export PATH=~/.bin:/usr/local/bin:~/.cabal/bin:~/.cljr/bin:$PATH
 export NODE_PATH=/usr/local/lib/node:/usr/local/lib/node_modules
-
-# Git pull from other repositories
-alias gitpulllin='git pull ssh://thomas@lucy/~ master'
-alias gitpullwin='git pull file:///Volumes/dotfiles/ master'
 
 # A secure tunnel to a KUL workstation, to be used in public hot
 # spots of the KUL
@@ -36,7 +33,6 @@ pman () {
 # View output in Preview.app with the preview command (usage: .. | preview)
 alias preview='groff -Tps > /tmp/tmp.ps && open -a Preview /tmp/tmp.ps'
 
-
 # Load autojump
 source /usr/local/Cellar/autojump/17/etc/autojump.zsh
 
@@ -45,11 +41,11 @@ alias br='brew'
 alias bri='brew install'
 alias brs='brew search'
 alias bru='brew update'
+alias brup='brew upgrade'
 alias brrm='brew uninstall'
 alias bro='brew outdated'
 alias brdeps='brew deps'
 alias bruses='brew uses'
-# TODO upgrade?
 
 
 # Easy way to mount my samba share
@@ -69,3 +65,4 @@ mnt_win () {
     mount_smbfs "//Thomas:$password@192.168.1.3/$1" "/Volumes/$1"
   fi
 }
+
