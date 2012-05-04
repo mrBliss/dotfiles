7eb07d09ed8cdaa8e5e9be911c32f3f41592218a
mrBliss <dewinant@gmail.com>
2012-05-04 14:33:42 +0200

    Better highlighting of search results in less.

--- .oh-my-zsh/lib/appearance.zsh
+++ .oh-my-zsh/lib/appearance.zsh
@@ -28,7 +28,7 @@ else
     export LESS_TERMCAP_md=$'\E[01;38;5;74m' # begin bold
     export LESS_TERMCAP_me=$'\E[0m' # end mode
     export LESS_TERMCAP_se=$'\E[0m' # end standout-mode
-    export LESS_TERMCAP_so=$'\E[38;5;246m' # begin standout-mode - info box
+    export LESS_TERMCAP_so=$'\E[44;37m' # begin standout-mode - info box
     export LESS_TERMCAP_ue=$'\E[0m' # end underline
     export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
 
