d081ef404c56211146987d42aa97a9f2bf6e6f2f
mrBliss <dewinant@gmail.com>
2012-06-04 21:45:22 +0200

    Backport of string-prefix-p for Emacs 23.1

--- .emacs.d/specific.el
+++ .emacs.d/specific.el
@@ -77,4 +77,14 @@
   (global-subword-mode 1))
 
 
+;; Backport to Emacs 23.1
+(when (version< emacs-version "23.2")
+  (defun string-prefix-p (str1 str2 &optional ignore-case)
+    "Return non-nil if STR1 is a prefix of STR2.
+If IGNORE-CASE is non-nil, the comparison is done without paying attention
+to case differences."
+    (eq t (compare-strings str1 nil nil
+                           str2 0 (length str1) ignore-case))))
+
+
 (provide 'specific)
