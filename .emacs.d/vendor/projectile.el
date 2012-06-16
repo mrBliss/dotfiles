b5820a9375af277315f24e539a7eea3ee124038f
mrBliss <dewinant@gmail.com>
2012-06-16 21:11:10 +0200

    Update projectile.

--- .emacs.d/vendor/projectile.el
+++ .emacs.d/vendor/projectile.el
@@ -4,7 +4,7 @@
 
 ;; Author: Bozhidar Batsov
 ;; URL: https://github.com/bbatsov/projectile
-;; Version: 0.6
+;; Version: 0.7
 ;; Created: 2011-31-07
 ;; Keywords: project, convenience
 
@@ -52,12 +52,21 @@
   :group 'projectile
   :type 'boolean)
 
+(defcustom projectile-keymap-prefix (kbd "C-c p")
+  "Projectile keymap prefix."
+  :group 'projectile
+  :type 'sexp)
+
+
 ;; variables
-(defvar projectile-project-root-files '(".git" ".hg" ".bzr" ".projectile")
+(defvar projectile-project-root-files '(".git" ".hg" ".bzr" "_darcs" ".projectile")
   "A list of files considered to mark the root of a project.")
 
 (defvar projectile-ignored-files '("TAGS")
   "A list of files ignored by projectile.")
+
+(defvar projectile-ignored-directories '(".idea")
+  "A list of directories ignored by projectile.")
 
 (defvar projectile-ignored-file-extensions '("class" "o" "so" "elc")
   "A list of file extensions ignored by projectile.")
@@ -65,7 +74,7 @@
 (defvar projectile-projects-cache (make-hash-table :test 'equal)
   "A hashmap used to cache project file names to speed up related operations.")
 
-(defun projectile-invalidate-project-cache ()
+(defun projectile-invalidate-cache ()
   "Remove the current project's files from `projectile-projects-cache'."
   (interactive)
   (let ((project-root (projectile-get-project-root)))
@@ -78,34 +87,40 @@
         when (locate-dominating-file default-directory file)
         do (return it)))
 
+(defun projectile-get-project-name ()
+  "Return project name."
+  (file-name-nondirectory (directory-file-name (projectile-get-project-root))))
+
 (defun projectile-get-project-files (directory)
-  "List the files in `DIRECTORY' and in its sub-directories."
+  "List the files in DIRECTORY and in its sub-directories."
   ;; check for a cache hit first if caching is enabled
   (let ((files-list (and projectile-enable-caching
-                     (gethash directory projectile-projects-cache))))
+                         (gethash directory projectile-projects-cache))))
     ;; cache miss
     (unless files-list
       ;; while we are in the current directory
       (dolist (current-file (file-name-all-completions "" directory) files-list)
-        (cond
-         ;; check for directories that are not ignored
-         ((and (projectile-string-suffix-p current-file "/")
-               (not (or (string= current-file "./") (string= current-file "../")))
-               (not (projectile-ignored-p (concat directory current-file))))
-          (setq files-list (append files-list (projectile-get-project-files (concat directory current-file)))))
-         ;; check for regular files that are not ignored
-         ((and (not (or (string= current-file "./") (string= current-file "../")))
-               (not (projectile-string-suffix-p current-file "/"))
-               (not (projectile-ignored-extension-p current-file))
-               (not (projectile-ignored-file-p current-file)))
-          (setq files-list (cons (expand-file-name (concat directory current-file)) files-list)))))
-      ;; cache the resulting list of files
-      (when (and projectile-enable-caching (string= directory (projectile-get-project-root)))
-        (puthash directory files-list projectile-projects-cache)))
+        (let ((absolute-file (file-name-as-directory (expand-file-name current-file directory))))
+          (cond
+           ;; check for directories that are not ignored
+           ((and (projectile-string-suffix-p current-file "/")
+                 (not (or (string= current-file "./") (string= current-file "../")))
+                 (not (projectile-ignored-p absolute-file))
+                 (not (projectile-ignored-directory-p absolute-file)))
+            (setq files-list (append files-list (projectile-get-project-files (concat directory current-file)))))
+           ;; check for regular files that are not ignored
+           ((and (not (or (string= current-file "./") (string= current-file "../")))
+                 (not (projectile-string-suffix-p current-file "/"))
+                 (not (projectile-ignored-extension-p current-file))
+                 (not (projectile-ignored-file-p absolute-file))
+                 (setq files-list (cons (expand-file-name (concat directory current-file)) files-list))))))
+        ;; cache the resulting list of files
+        (when (and projectile-enable-caching (string= directory (projectile-get-project-root)))
+          (puthash directory files-list projectile-projects-cache))))
     files-list))
 
 (defun projectile-string-suffix-p (string suffix)
-  "Check whether `STRING' ends with `SUFFIX'."
+  "Check whether STRING ends with SUFFIX."
   (string= (substring string (- (length string) (length suffix))) suffix))
 
 (defun projectile-get-project-buffers ()
@@ -118,10 +133,16 @@
   "Get a list of project buffer names."
   (mapcar 'buffer-name (projectile-get-project-buffers)))
 
+(defun projectile-prepend-project-name (string)
+  (format "[%s] %s" (projectile-get-project-name) string))
+
 (defun projectile-switch-to-buffer ()
   "Switch to a project buffer."
   (interactive)
-  (switch-to-buffer (ido-completing-read "Jump to project buffer: " (projectile-get-project-buffer-names))))
+  (switch-to-buffer
+   (ido-completing-read
+    (projectile-prepend-project-name "Switch to buffer: ")
+    (projectile-get-project-buffer-names))))
 
 (defun projectile-multi-occur ()
   "Do a `multi-occur' in the project's buffers."
@@ -130,61 +151,110 @@
                (car (occur-read-primary-args))))
 
 (defun projectile-hashify-files (files-list)
-  "Make the list of project files `FILES-LIST' ido friendly."
+  "Make the list of project files FILES-LIST ido friendly."
   (let ((files-table (make-hash-table :test 'equal))
         (files-to-uniquify nil))
     (dolist (current-file files-list files-table)
       (let ((basename (file-name-nondirectory current-file)))
         (if (gethash basename files-table)
             (progn
-              (puthash (uniquify-file current-file) current-file files-table)
+              (puthash (projectile-uniquify-file current-file) current-file files-table)
               (when basename (push basename files-to-uniquify)))
           (puthash basename current-file files-table))))
     ;; uniquify remaining files
     (dolist (current-file (remove-duplicates files-to-uniquify :test 'string=))
-      (puthash (uniquify-file (gethash current-file files-table)) (gethash current-file files-table) files-table)
+      (puthash (projectile-uniquify-file (gethash current-file files-table)) (gethash current-file files-table) files-table)
       (remhash current-file files-table))
     files-table))
 
-(defun uniquify-file (filename)
-  "Create an unique version of a `FILENAME'."
+(defun projectile-uniquify-file (filename)
+  "Create an unique version of a FILENAME."
   (let ((filename-parts (reverse (split-string filename "/"))))
     (format "%s/%s" (second filename-parts) (first filename-parts))))
 
 (defun projectile-ignored-p (file)
-  "Check if `FILE' should be ignored."
-  (loop for ignored in projectile-project-root-files
-        when (string= (expand-file-name (concat (projectile-get-project-root) ignored "/")) (expand-file-name file))
-        do (return t)
-        finally (return nil)))
+  "Check if FILE should be ignored."
+  (find-if
+   (lambda (root-file)
+     (string= file (projectile-expand-root root-file)))
+   projectile-project-root-files))
+
+(defun projectile-ignored-directory-p (directory)
+  "Check if DIRECTORY should be ignored."
+  (member directory (projectile-ignored-directories)))
 
 (defun projectile-ignored-file-p (file)
-  "Check if `FILE' should be ignored."
-  (member file projectile-ignored-files))
+  "Check if FILE should be ignored."
+  (member file (projectile-ignored-files)))
 
 (defun projectile-ignored-extension-p (file)
-  "Check if `FILE' should be ignored based on its extension."
+  "Check if FILE should be ignored based on its extension."
   (let ((ext (file-name-extension file)))
     (member ext projectile-ignored-file-extensions)))
 
-(defun projectile-jump-to-project-file ()
+(defun projectile-ignored-files ()
+  "Return list of ignored files."
+  (mapcar
+   'projectile-expand-root
+   (append projectile-ignored-files (projectile-project-ignored-files))))
+
+(defun projectile-ignored-directories ()
+  "Return list of ignored directories."
+  (mapcar
+   'projectile-expand-root
+   (append projectile-ignored-directories (projectile-project-ignored-directories))))
+
+(defun projectile-project-ignored-files ()
+  "Return list of project ignored files."
+  (delete-if 'file-directory-p (projectile-project-ignored)))
+
+(defun projectile-project-ignored-directories ()
+  "Return list of project ignored directories."
+  (delete-if-not 'file-directory-p (projectile-project-ignored)))
+
+(defun projectile-project-ignored ()
+  "Return list of project ignored files/directories."
+  (let ((patterns (projectile-parse-ignore-file))
+        (default-directory (projectile-get-project-root)))
+    (apply 'append (mapcar (lambda (pattern) (file-expand-wildcards pattern t)) patterns))))
+
+(defun projectile-parse-ignore-file ()
+  "Parse project ignore file and return list of ignores."
+  (let ((ignore-file (expand-file-name ".projectile" (projectile-get-project-root))))
+    (when (file-exists-p ignore-file)
+      (with-temp-buffer
+        (insert-file-contents-literally ignore-file)
+          (mapcar 'projectile-trim (delete "" (split-string (buffer-string) "\n")))))))
+
+(defun projectile-trim (string)
+  "Return STRING with whitespace removed from front and back."
+  (replace-regexp-in-string "^[ \t\n]+" "" (replace-regexp-in-string "[ \t\n]+$" "" string)))
+
+(defun projectile-expand-root (name)
+  "Expand NAME to project root."
+  (file-name-as-directory (expand-file-name name (projectile-get-project-root))))
+
+(defun projectile-find-file ()
   "Jump to a project's file using ido."
   (interactive)
   (let* ((project-files (projectile-hashify-files
                          (projectile-get-project-files (projectile-get-project-root))))
-         (file (ido-completing-read "Jump to project file: "
+         (file (ido-completing-read (projectile-prepend-project-name "File file: ")
                                     (loop for k being the hash-keys in project-files collect k))))
     (find-file (gethash file project-files))))
 
-(defun projectile-grep-in-project ()
+(defun projectile-grep ()
   "Perform rgrep in the project."
   (interactive)
   (let ((search-regexp (if (and transient-mark-mode mark-active)
                            (buffer-substring (region-beginning) (region-end))
-                         (read-string "Search for: " (thing-at-point 'symbol))))
+                         (read-string (projectile-prepend-project-name "Grep for: ") (thing-at-point 'symbol))))
         (root-dir (projectile-get-project-root)))
-    (grep-compute-defaults)
-    (rgrep search-regexp "* .*" root-dir)))
+    (require 'grep)
+    (let ((grep-find-ignored-directories (append projectile-ignored-directories grep-find-ignored-directories))
+          (grep-find-ignored-files (append (projectile-ignored-files) grep-find-ignored-files)))
+      (grep-compute-defaults)
+      (rgrep search-regexp "* .*" root-dir))))
 
 (defun projectile-regenerate-tags ()
   "Regenerate the project's etags using ctags."
@@ -196,7 +266,7 @@
     (cd current-dir)
     (visit-tags-table project-root)))
 
-(defun projectile-replace-in-project ()
+(defun projectile-replace ()
   "Replace a string in the project using perl."
   (interactive)
   (let ((current-dir default-directory)
@@ -205,32 +275,51 @@
         (new-text (read-string "With: ")))
     (shell-command (format "find %s -type d -name .git -prune -o -print| xargs perl -p -i -e 's/%s/%s/g'" project-root old-text new-text))))
 
+(defun projectile-kill-buffers ()
+  "Kill all project buffers."
+  (interactive)
+  (let* ((buffers (projectile-get-project-buffer-names))
+         (question
+          (format
+           "Are you sure you want to kill %d buffer(s) for '%s'? "
+           (length buffers)
+           (projectile-get-project-name))))
+    (if (yes-or-no-p question)
+        (mapc 'kill-buffer buffers))))
+
 (defvar projectile-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c p j") 'projectile-jump-to-project-file)
-    (define-key map (kbd "C-c p f") 'projectile-grep-in-project)
-    (define-key map (kbd "C-c p b") 'projectile-switch-to-buffer)
-    (define-key map (kbd "C-c p o") 'projectile-multi-occur)
-    (define-key map (kbd "C-c p r") 'projectile-replace-in-project)
-    (define-key map (kbd "C-c p i") 'projectile-invalidate-project-cache)
-    (define-key map (kbd "C-c p t") 'projectile-regenerate-tags)
-    map)
+      (let ((prefix-map (make-sparse-keymap)))
+        (define-key prefix-map (kbd "f") 'projectile-find-file)
+        (define-key prefix-map (kbd "g") 'projectile-grep)
+        (define-key prefix-map (kbd "b") 'projectile-switch-to-buffer)
+        (define-key prefix-map (kbd "o") 'projectile-multi-occur)
+        (define-key prefix-map (kbd "r") 'projectile-replace)
+        (define-key prefix-map (kbd "i") 'projectile-invalidate-cache)
+        (define-key prefix-map (kbd "t") 'projectile-regenerate-tags)
+        (define-key prefix-map (kbd "k") 'projectile-kill-buffers)
+
+        (define-key map projectile-keymap-prefix prefix-map)
+
+        ;; shortcuts without prefices
+        )
+      map)
   "Keymap for Projectile mode.")
 
 (easy-menu-define projectile-mode-menu projectile-mode-map
   "Menu for Projectile mode"
   '("Projectile"
     ("Navigating"
-     ["Jump to file" projectile-jump-to-project-file]
-     ["Jump to buffer" projectile-switch-to-buffer])
+     ["File file" projectile-find-file]
+     ["Switch to buffer" projectile-switch-to-buffer])
 
     ("Find & Replace"
-     ["Find in project" projectile-grep-in-project]
-     ["Replace in project" projectile-replace-in-project]
+     ["Find in project" projectile-grep]
+     ["Replace in project" projectile-replace]
      ["Multi-occur in project" projectile-multi-occur])
 
     ("General"
-     ["Invalidate cache" projectile-invalidate-project-cache]
+     ["Invalidate cache" projectile-invalidate-cache]
      ["Regenerate etags" projectile-regenerate-tags])))
 
 ;; define minor mode
@@ -248,8 +337,9 @@
 
 ;;;###autoload
 (define-minor-mode projectile-mode "Minor mode to assist project management and navigation."
-  :lighter " Projectile"
+  :lighter " P"
   :keymap projectile-mode-map
+  :group 'projectile
   (if projectile-mode
       ;; on start
       (easy-menu-add projectile-mode-menu projectile-mode-map)
