5ad29ae234e53887f99539a993f96bb960d4e2d0
mrBliss <dewinant@gmail.com>
2012-09-19 20:11:34 +0200

    Switch back to the default powerline theme
    Window-numbering-mode adds itself to the mode line anyway.

--- .emacs.d/custom-appearance.el
+++ .emacs.d/custom-appearance.el
@@ -49,67 +49,8 @@ a light color-theme when passed a prefix argument."
 (setq color-theme-is-global nil)
 
 
-
 (require 'powerline)
-
-;; Include the window-number
-(defpowerline powerline-window-number
-  (window-numbering-get-number-string))
-
-(defun powerline-my-theme ()
-  "Setup my mode-line."
-  (interactive)
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (face1 (if active 'powerline-active1
-                                   'powerline-inactive1))
-                          (face2 (if active 'powerline-active2
-                                   'powerline-inactive2))
-                          (lhs (list
-                                (powerline-window-number nil 'l)
-                                (powerline-raw "%*" nil 'l)
-                                (powerline-buffer-size nil 'l)
-                                (powerline-buffer-id nil 'l)
-
-                                (powerline-raw " ")
-                                (powerline-arrow-right nil face1)
-
-                                (when (boundp 'erc-modified-channels-object)
-                                  (powerline-raw erc-modified-channels-object
-                                                 face1 'l))
-
-                                (powerline-major-mode face1 'l)
-                                (powerline-process face1)
-                                (powerline-minor-modes face1 'l)
-                                (powerline-narrow face1 'l)
-
-                                (powerline-raw " " face1)
-                                (powerline-arrow-right face1 face2)
-
-                                (powerline-vc face2)))
-                          (rhs (list
-                                (powerline-raw global-mode-string face2 'r)
-
-                                (powerline-arrow-left face2 face1)
-
-                                (powerline-raw "%4l" face1 'r)
-                                (powerline-raw ":" face1)
-                                (powerline-raw "%3c" face1 'r)
-
-                                (powerline-arrow-left face1 nil)
-                                (powerline-raw " ")
-
-                                (powerline-raw "%6p" nil 'r)
-
-                                (powerline-hud face2 face1))))
-                     (concat
-                      (powerline-render lhs)
-                      (powerline-fill face2 (powerline-width rhs))
-                      (powerline-render rhs)))))))
-
-(powerline-my-theme)
+(powerline-default)
 
 
 (defvar mode-line-cleaner-alist
