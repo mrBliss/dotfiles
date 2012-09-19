6f23645cc3b71fc1afac6b6b52c8a0aee28308af
mrBliss <dewinant@gmail.com>
2012-09-19 20:13:19 +0200

    Haskell-mode customisations

--- .emacs.d/custom-haskell.el
+++ .emacs.d/custom-haskell.el
@@ -9,6 +9,7 @@
 (load-file "~/.emacs.d/vendor/haskell-mode/haskell-site-file.el")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 
 ;; ghc-mod
 (autoload 'ghc-init "ghc" nil t)
@@ -19,6 +20,60 @@
 
 ;; Pretty unicode symbols (messes up indentation for other people)
 (setq haskell-font-lock-symbols t)
+
+(defun haskell-indent-insert-comma ()
+  "Insert a comma and indent.  Start a new line unless the current
+line is blank."
+  (interactive)
+  ;; Not on an empty line, start a new line
+  (when (save-excursion (beginning-of-line) (not (looking-at "\\s-*$")))
+    (newline))
+  (insert ",")
+  (backward-char 1)
+  (haskell-indent-cycle)
+  (forward-char 1)
+  (insert " "))
+
+(defun haskell-next-argument ()
+  "Insert the arrow (\" -> \") so the user can type the next
+  argument. Takes care of already present spaces."
+  (interactive)
+  (delete-trailing-whitespace (line-beginning-position)
+                              (line-end-position))
+  ;; If there's already an arrow, don't add another one
+  (if (save-excursion
+            (backward-char 2)
+            (looking-at "->"))
+      (insert " ")
+    (insert " -> ")))
+
+(defun haskell-implement-or-next-case ()
+  "Start the implementation on the next line of the function
+signature on the current line.  If the current line contains a
+case of an implementation, start a new case on the next line.  The
+point can also be on an empty line, in that case, the line above
+is looked at."
+  (interactive)
+  ;; On an empty line, go back a line until we find a non-empty line.
+  (while (save-excursion (beginning-of-line) (looking-at "\\s-*$"))
+    (forward-line -1))
+  ;; To move one line down, there needs to be one.
+  (when (eobp) (newline))
+  (forward-line)
+  ;; If we're not on an empty line (perhaps after the sig, but before
+  ;; case), make place for the implementation or case.
+  (unless (looking-at "\\s-*$")
+    (newline)
+    (forward-line -1))
+  ;; Find the name of the function we're trying to implement or add a
+  ;; case for.
+  (let ((decl-name (save-excursion
+                     (haskell-ds-backward-decl)
+                     (caar (haskell-ds-generic-find-next-decl nil)))))
+    ;; Insert the implementation/case
+    (insert decl-name " ")))
+
+
 
 (defun haskell-hook ()
   (turn-on-haskell-indent)
@@ -31,8 +86,12 @@
   (define-key haskell-mode-map (kbd "M-P") 'flymake-goto-prev-error)
   (define-key haskell-mode-map (kbd "M-n") 'forward-paragraph)
   (define-key haskell-mode-map (kbd "M-p") 'backward-paragraph)
-  (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-tag-find)
-  (define-key haskell-mode-map (kbd "C-c c-w") 'haskell-indent-insert-where)
+  (define-key haskell-mode-map (kbd "M-.") 'find-tag)
+  ;;  (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-tag-find)
+  (define-key haskell-mode-map (kbd "C-c C-,") 'haskell-indent-insert-comma)
+  (define-key haskell-mode-map (kbd "C-c C-a") 'haskell-next-argument)
+  (define-key haskell-mode-map (kbd "C-c C-n") 'haskell-implement-or-next-case)
+  ;;  (define-key haskell-mode-map (kbd "C-x C-s") 'haskell-mode-save-buffer)
   (setq ghc-ghc-options '("-XGADTs" "-XKindSignatures" "-XTypeFamilies"))
   (setq haskell-program-name "ghci -XGADTs -XKindSignatures -XTypeFamilies")
   (flymake-mode 1))
