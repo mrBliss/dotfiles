e553ef6c3f79ad7dc69391c411e3fb26e221bb43
mrBliss <dewinant@gmail.com>
2012-12-10 12:54:01 +0100

    Don't display the menu bar

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,6 +13,7 @@
 ;; You really don't need these; trust me.
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 ;; Turn off welcome screen
 (setq inhibit-startup-message t)
@@ -84,4 +85,3 @@
 
 ;; ECB ignores setq
 (custom-set-variables '(ecb-options-version "2.40"))
-
