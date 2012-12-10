f48ac51a9f3f73fa789865820ab565aae956a062
mrBliss <dewinant@gmail.com>
2012-12-10 12:51:16 +0100

    Use Evince as default PDF viewer on GNU/Linux

--- .emacs.d/custom-latex.el
+++ .emacs.d/custom-latex.el
@@ -40,11 +40,11 @@
   ;; Quickly type {|} with | being the point.
   (key-chord-define LaTeX-mode-map "((" "{}\C-b")
 
-  ;; Use Zathura as default PDF viewer on GNU/Linux, Skim on OS X.
+  ;; Use evince as default PDF viewer on GNU/Linux, Skim on OS X.
   (setq TeX-view-program-list
         (list (if (eq system-type 'darwin)
                   '("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b %n %o %b")
-                '("Zathura" "zathura %o"))))
+                '("Evince" "evince %o"))))
   (push (list 'output-pdf (caar TeX-view-program-list)) TeX-view-program-selection))
 
 
