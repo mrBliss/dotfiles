1544adbc207e55aea8ecae21aa034ee761ec4dc6
mrBliss <dewinant@gmail.com>
2013-02-16 17:30:56 +0100

    Magit-toggle-whitespace

--- .emacs.d/custom-misc.el
+++ .emacs.d/custom-misc.el
@@ -236,8 +236,27 @@
 (autoload 'git-commit-mode-magit-setup "git-commit-mode" nil t)
 
 (eval-after-load "magit"
-  '(add-hook 'magit-log-edit-mode-hook
-             'git-commit-mode-magit-setup))
+  '(progn
+     (add-hook 'magit-log-edit-mode-hook
+               'git-commit-mode-magit-setup)
+
+     (defun magit-toggle-whitespace ()
+       (interactive)
+       (if (member "-w" magit-diff-options)
+           (magit-dont-ignore-whitespace)
+         (magit-ignore-whitespace)))
+
+     (defun magit-ignore-whitespace ()
+       (interactive)
+       (add-to-list 'magit-diff-options "-w")
+       (magit-refresh))
+
+     (defun magit-dont-ignore-whitespace ()
+       (interactive)
+       (setq magit-diff-options (remove "-w" magit-diff-options))
+       (magit-refresh))
+
+     (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)))
 
 
 
